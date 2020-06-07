d2c2fd8c46ab9b53f35a29691b9644e0d399cb9f
Intexisty <legoguy253@gmail.com>
2020-06-07 15:08:58 -0500

    created incremen

---
+++ foster_c_by_discovery/Chapter 1 - Getting Started/incremen/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3, b;
+
+    b = a++;
+    printf("b is %d, and a is %d.\n", b, a);
+
+    b = 5 % --a;
+    printf("Now b is %d, and a is %d.\n", b, a);
+
+    printf("now b is %d, and a is %d.\n", ++b, a--);
+    printf("now b is %d, and a is %d.\n", b, a);
+    
+    return 0;
+}
