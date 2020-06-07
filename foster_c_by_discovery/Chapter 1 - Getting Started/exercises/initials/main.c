d3590cdbd2c3f413203310458dec9c47d35eb87f
Intexisty <legoguy253@gmail.com>
2020-06-07 15:23:26 -0500

    created initials

---
+++ foster_c_by_discovery/Chapter 1 - Getting Started/exercises/initials/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("    _          _   ____ \n");
+    printf("   / \\        | | / ___|\n");
+    printf("  / _ \\    _  | || |    \n");
+    printf(" / ___ \\  | |_| || |___ \n");
+    printf("/_/   \\_\\ \\____/  \\____|\n");
+    return 0;
+}
