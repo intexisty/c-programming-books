62f46f229d17d95757210cad4d9e28f17c1576ee
Intexisty <legoguy253@gmail.com>
2020-06-07 14:42:26 -0500

    created intvar

---
+++ foster_c_by_discovery/Chapter 1 - Getting Started/intvar/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int first;
+    int second = 2;
+    int third, fourth = 4;
+    first = 1;
+    third = 3;
+
+    printf("first is %d, ", first);
+    printf("second is %d, third is %d, and fourth is %d\n", second, third, fourth);
+    return 0;
+}
